a9434b8e72f1cc9d54f297502be9e8d8268bcd83
UWABOR COLLINS <99263767+Beinglegendary@users.noreply.github.com>
2023-03-17 04:33:53 +0100

    GetDemo

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import GetDemo from "./getDemo";
+
+function App() {
+  return (
+    <div className="App">
+      <GetDemo />
+    </div>
+  );
+}
+
+export default App;
