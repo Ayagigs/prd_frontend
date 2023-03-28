1deeb3297be6f7d55324e89ca4396f1645b4dd36
Sophia Okosodo <sophieokosodo@gmail.com>
2023-03-28 13:16:56 +0100

    made changes

--- src/empdashboard/components/profiletab/Profiletab.jsx
+++ src/empdashboard/components/profiletab/Profiletab.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import "./profiletab.scss";
 import { Link } from "react-router-dom";
 import ReactDOM from 'react-dom';
-import { Carousel } from '@trendyol-js/react-carousel';
+// import { Carousel } from '@trendyol-js/react-carousel';
 // import { Item } from './yourItem';
 
 const Profiletab = () => {
