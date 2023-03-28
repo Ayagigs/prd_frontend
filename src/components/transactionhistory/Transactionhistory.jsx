5277639ddacf5f3cbc426bf0198d5b49b3fa934a
“Paul <paulpelumi@gmail.com>
2023-03-28 16:12:29 +0100

    wallet page update

--- src/components/transactionhistory/Transactionhistory.jsx
+++ src/components/transactionhistory/Transactionhistory.jsx
@@ -1,4 +1,4 @@
-import "./table.scss";
+import "./transactionhistory.scss";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -6,88 +6,58 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import { ArrangeHorizontal } from 'iconsax-react';
+import { Wallet2 } from 'iconsax-react';
 
-const List = () => {
+const Transactionhistory = () => {
   const rows = [
     {
-      id: 1143155,
-      product: "Acer Nitro 5",
-      img: "https://m.media-amazon.com/images/I/81bc8mA3nKL._AC_UY327_FMwebp_QL65_.jpg",
-      customer: "John Smith",
-      date: "1 March",
-      amount: 785,
-      method: "Cash on Delivery",
-      status: "inactive",
+      transaction: "Sent to Kemik@gmail.com",
+      date: "March 6, 2023. 2:30 AM",
+      amount: "90,450.00 USD",
+      status: "pending",
     },
     {
-      id: 2235235,
-      product: "Playstation 5",
-      img: "https://m.media-amazon.com/images/I/31JaiPXYI8L._AC_UY327_FMwebp_QL65_.jpg",
-      customer: "Michael Doe",
-      date: "1 March",
-      amount: 900,
-      method: "Online Payment",
-      status: "inactive",
+      transaction: "Sent to Kemik@gmail.com",
+      date: "March 6, 2023. 2:30 AM",
+      amount: "90,450.00 USD",
+      status: "cancelled",
     },
     {
-      id: 2342353,
-      product: "Redragon S101",
-      img: "https://m.media-amazon.com/images/I/71kr3WAj1FL._AC_UY327_FMwebp_QL65_.jpg",
-      customer: "John Smith",
-      date: "1 March",
-      amount: 35,
-      method: "Cash on Delivery",
-      status: "active",
+      transaction: "Sent to Kemik@gmail.com",
+      date: "March 6, 2023. 2:30 AM",
+      amount: "90,450.00 USD",
+      status: "successful",
     },
     {
-      id: 2357741,
-      product: "Razer Blade 15",
-      img: "https://m.media-amazon.com/images/I/71wF7YDIQkL._AC_UY327_FMwebp_QL65_.jpg",
-      customer: "Jane Smith",
-      date: "1 March",
-      amount: 920,
-      method: "Online",
-      status: "inactive",
+      transaction: "Sent to Kemik@gmail.com",
+      date: "March 6, 2023. 2:30 AM",
+      amount: "90,450.00 USD",
+      status: "pending",
     },
     {
-      id: 2342355,
-      product: "ASUS ROG Strix",
-      img: "https://m.media-amazon.com/images/I/81hH5vK-MCL._AC_UY327_FMwebp_QL65_.jpg",
-      customer: "Harold Carol",
-      date: "1 March",
-      amount: 2000,
-      method: "Online",
-      status: "active",
+      transaction: "Sent to Kemik@gmail.com",
+      date: "March 6, 2023. 2:30 AM",
+      amount: "90,450.00 USD",
+      status: "successful",
     },
+
+  
   ];
   return (
-    <TableContainer component={Paper} className="table">
+    <TableContainer component={Paper} className="transtable">
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead className="tablehead"> 
-          <TableRow>
-            <TableCell className="tableCell">Employee ID</TableCell>
-            <TableCell className="tableCell">Employee Name</TableCell>
-            <TableCell className="tableCell">Email Address</TableCell>
-            <TableCell className="tableCell">Date joined</TableCell>
-            <TableCell className="tableCell">Achieved goals</TableCell>
-            <TableCell className="tableCell">Payment Method</TableCell>
-            <TableCell className="tableCell">Status</TableCell>
-          </TableRow>
-        </TableHead>
         <TableBody>
           {rows.map((row) => (
-            <TableRow key={row.id}>
-              <TableCell className="tableCell">{row.id}</TableCell>
+            <TableRow key={row.transaction}>
               <TableCell className="tableCell">
                 <div className="cellWrapper">
-                  <img src={row.img} alt="" className="image" />
-                  {row.product}
+                <ArrangeHorizontal size="15" color="#5157ed" />
+                  {row.transaction}
                 </div>
+                <TableCell className="transdate">{row.date}</TableCell>
               </TableCell>
-              <TableCell className="tableCell">{row.customer}</TableCell>
-              <TableCell className="tableCell">{row.date}</TableCell>
               <TableCell className="tableCell">{row.amount}</TableCell>
-              <TableCell className="tableCell">{row.method}</TableCell>
               <TableCell className="tableCell">
                 <span className={`status ${row.status}`}>{row.status}</span>
               </TableCell>
@@ -95,8 +65,10 @@ const List = () => {
           ))}
         </TableBody>
       </Table>
+      <button className="viewallbtn">View all transactions</button>
     </TableContainer>
+
   );
 };
 
-export default List;
+export default Transactionhistory;
